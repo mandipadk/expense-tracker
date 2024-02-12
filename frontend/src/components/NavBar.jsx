29db16b41887b5c1f1d9830aa4b7d4d47c73a671
Gunjan Sah <sahgunjan04@gmail.com>
2024-02-12 15:31:20 -0600

    test

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <div>NavBar
+        <h1> This is a test code by Gunjan.</h1>
+        <h2> This is a test code by mandip.</h2>
+    </div>
+  )
+}
