b2cbf09430284bae5655865b6fb0f40195ebd52f
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-02-13 21:46:01 -0600

    Test commit

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,6 +6,7 @@ export const NavBar = () => {
         <h1> This is a test code by Gunjan.</h1>
         <h2> This is a test code by mandip.</h2>
         <h2>This is also a test code</h2>
+        <h2>This is a code by Aditya</h2>
     </div>
   )
 }
