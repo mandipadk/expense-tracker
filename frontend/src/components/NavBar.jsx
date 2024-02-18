2f8853c2f2d9b7f8a17fdec5b077c5a218ca3323
Aditya Sharma <aditya2002.05.19@gmail.com>
2024-02-18 16:52:55 -0600

    looo

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ export const NavBar = () => {
         <h2>This is also a test code</h2>
         <h2>This is a code by Aditya</h2>
         <h2> This is another code by Kapil</h2>
-        <h3> this is another code by meee</h3>
+        <h3> this is another code by new version</h3>
     </div>
   )
 }
